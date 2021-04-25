1908ea8223e37d6a4576d029b331446f046f4e2f
Mukulbaid63 <mukulbaid63@gmail.com>
2021-04-26 00:44:49 +0545

    Styling improvements

--- src/components/About.js
+++ src/components/About.js
@@ -2,9 +2,9 @@ import React from 'react';
 import "../styles/About.css"
 const About = () => {
     return (
-        <div className="text-light p-4" style={{flex:'0.3',width:'20vw'}} id="about">
+        <div className="text-light p-4" style={{flex:'0.3'}} id="about">
             
-            <h2>ABOUT:&nbsp; </h2>
+            <h2 className="mb-4 border-bottom border-white border-1">ABOUT:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </h2>
             <p>a suscipit nisi nisl non mi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur euismod fermentum finibus. Phasellus tempus enim sed magna euismod rutrum. Aliquam erat volutpat. Nulla euismod felis eu ultrices euismod.</p>
             </div>
     );
