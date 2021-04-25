35a835882e20c9440d58ae01726870072d06dbfc
Mukulbaid63 <mukulbaid63@gmail.com>
2021-04-26 00:55:38 +0545

    MORE IMPS

--- src/components/ForecastWeather.js
+++ src/components/ForecastWeather.js
@@ -6,7 +6,7 @@ const ForecastWeather = ({ data }) => {
     <div
       className="text-light m-1 my-2 d-flex justify-content-between"
       id="forecast"
-      style={{ fontSize: "15px" }}
+      style={{ fontSize: "17px" }}
     >
       <div className="d-flex p-1 flex-column align-items-start">
         <div className="p-1">
@@ -18,19 +18,19 @@ const ForecastWeather = ({ data }) => {
           <i class="fas fa-tint"></i>
           <span className="px-2">{data.main.humidity} %</span>
         </div>
-        <div className="p-1 fw-bolder">{data.dt_txt}</div>
+        <div className="p-1 fw-bolder" style={{ textShadow: "2px 2px rgba(34, 33, 33, 0.6)",}}>{data.dt_txt}</div>
       </div>
       <div className="p-1 d-flex flex-column align-items-end">
         <div
           style={{
             fontSize: "40px",
             fontWeight: "500",
-            textShadow: "2px 3px rgba(34, 33, 33, 0.6)",
+            textShadow: "2px 3px rgba(34, 33, 33, 0.6)"
           }}
         >
           {Math.round(data.main.temp)}°c
         </div>
-        <div>{data.weather[0].main}</div>
+        <div style={{fontSize:'23px'}}>{data.weather[0].main}</div>
       </div>
     </div>
   );
--- src/components/WeatherMenu.js
+++ src/components/WeatherMenu.js
@@ -1,79 +1,87 @@
-import React, { useState } from 'react';
-import {connect} from 'react-redux';
-import { fetchData } from '../redux/actions';
-import { fetchCurrentData } from '../redux/actionsCurrent';
-import CurrentWeather from './CurrentWeather';
-import ForecastWeather from './ForecastWeather';
-import '../styles/WeatherMenu.css'
-import About from './About';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { fetchData } from "../redux/actions";
+import { fetchCurrentData } from "../redux/actionsCurrent";
+import CurrentWeather from "./CurrentWeather";
+import ForecastWeather from "./ForecastWeather";
+import "../styles/WeatherMenu.css";
+import About from "./About";
+import { Link } from "react-router-dom";
 const WeatherMenu = (props) => {
-    const [about, setAbout] = useState(false)
-    const aboutHandler=()=>{
-        setAbout(!about);
+  const [about, setAbout] = useState(false);
+  const aboutHandler = () => {
+    setAbout(!about);
+  };
+  const changeHandler = (evt) => {
+    if (evt.target.value === "dummy") alert("SELECT A CITY");
+    else {
+      props.fetchData(evt.target.value);
+      props.fetchCurrentData(evt.target.value);
     }
-    const changeHandler=(evt)=>{
-        if(evt.target.value==="dummy") alert("SELECT A CITY");
-        else
-        {props.fetchData(evt.target.value);
-          props.fetchCurrentData(evt.target.value);
-        }
-      }
-      return (
-          <div className="d-flex" >
-        <div className="weather-app d-flex flex-column  text-light align-items-center justify-content-center">
-         <select onChange={changeHandler}>
-           <option value="dummy">Select a city</option>
-           <option value="Kolkata">Kolkata</option>
-           <option value="New Delhi">New Delhi</option>
-           <option value="Mumbai">Mumbai</option>
-           <option value="Chennai">Chennai</option>
-           <option value="Bangalore">Bangalore</option>
-           <option value="Srinagar">Srinagar</option>
-           <option value="Jaipur">Jaipur</option>
-           <option value="Ranchi">Ranchi</option>
-           <option value="Puducherry">Puducherry</option>
-
-
-    
-         </select>
-         {props.loading && <div className="text-light fw-bold">LOADING....</div>}
-         <div className="p-3 d-flex justify-content-around weather-ouput" style={{height:'38vh',width:'48vw'}}>
-         {props.currentWeather.main&& <CurrentWeather currentWeather={props.currentWeather}/>}
-         <div className="ml-3" style={{overflow:'auto'}}>{props.weather.map((item,id)=>(<ForecastWeather data={item}/>))}</div>
-         </div>
-         <div className="d-flex justify-content-between">
-            <button className="btn btn-outline-light mt-4 mx-4" onClick={aboutHandler}>About</button>
-            <Link to="/">
-        <button className="btn btn-outline-light mt-4 mx-4" >Logout</button>
-
-        </Link>
+  };
+  return (
+    <div className="d-flex">
+      <div className="weather-app d-flex flex-column  text-light align-items-center justify-content-center">
+        <select onChange={changeHandler}>
+          <option value="dummy">Select a city</option>
+          <option value="Kolkata">Kolkata</option>
+          <option value="New Delhi">New Delhi</option>
+          <option value="Mumbai">Mumbai</option>
+          <option value="Chennai">Chennai</option>
+          <option value="Bangalore">Bangalore</option>
+          <option value="Pune">Pune</option>
+          <option value="Srinagar">Srinagar</option>
+          <option value="Jaipur">Jaipur</option>
+          <option value="Ranchi">Ranchi</option>
+          <option value="Puducherry">Puducherry</option>
+        </select>
+        {props.loading && <div className="text-light fw-bold">LOADING....</div>}
+        <div
+          className="p-3 d-flex justify-content-around weather-ouput"
+          style={{ height: "38vh", width: "48vw" }}
+        >
+          {props.currentWeather.main && (
+            <CurrentWeather currentWeather={props.currentWeather} />
+          )}
+          <div className="ml-3" style={{ overflow: "auto" }}>
+            {props.weather.map((item, id) => (
+              <ForecastWeather data={item} />
+            ))}
+          </div>
         </div>
+        <div className="d-flex justify-content-between">
+          <button
+            className="btn btn-outline-light mt-4 mx-4"
+            onClick={aboutHandler}
+          >
+            About
+          </button>
+          <Link to="/">
+            <button className="btn btn-outline-light mt-4 mx-4">Logout</button>
+          </Link>
         </div>
-       
-        {about&&<About/>}
-        
-        
-        </div>
-      );
-    }
-    const mapStateToProps=(state)=>{
-      return{
-        loading:state.fetchDataReducer.loading,
-        weather:state.fetchDataReducer.weather,
-        currentWeather:state.fetchCurrentDataReducer.currentWeather,
-        error:state.fetchDataReducer.error
-      }
-    }
-    const mapDispatchToProps=(dispatch)=>{
-      return{
-        fetchData:(query)=>{
-          dispatch(fetchData(query))
-        },
-        fetchCurrentData:(query)=>{
-          dispatch(fetchCurrentData(query))
-        }
-      }
-    }
-    export default connect(mapStateToProps,mapDispatchToProps)(WeatherMenu);
-    
+      </div>
+
+      {about && <About />}
+    </div>
+  );
+};
+const mapStateToProps = (state) => {
+  return {
+    loading: state.fetchDataReducer.loading,
+    weather: state.fetchDataReducer.weather,
+    currentWeather: state.fetchCurrentDataReducer.currentWeather,
+    error: state.fetchDataReducer.error,
+  };
+};
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchData: (query) => {
+      dispatch(fetchData(query));
+    },
+    fetchCurrentData: (query) => {
+      dispatch(fetchCurrentData(query));
+    },
+  };
+};
+export default connect(mapStateToProps, mapDispatchToProps)(WeatherMenu);
